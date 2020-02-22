fb40f84d5858880993003c6cd13c86c86fa0e7b5
Remi LeBlanc <remwileblanc@gmail.com>
2020-02-22 16:39:26 -0500

    last name alphabetically

--- query.js
+++ query.js
@@ -14,10 +14,19 @@ query.exec(function(error, professors) {
 });*/
 
 const queries = [
-  // Voters from Canton
+  // How many registered voters live in the Canton zip code (13617)?
   Voter.find().where('zipcode').equals(13617),
+
+  // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('firstname').equals('STARR'),
-  Voter.find().where('history').in("GE16")
+
+  // How many people voted in the 2016 general election (GE16)?
+  Voter.find().where('history').in("GE16"),
+
+  // What is the last-name that comes last in the county in alphabetical order?
+  Voter.find().sort('-lastname').limit(1)
+
+  // How many zip codes does the county contain?
 ];
 
 Promise.all(queries)
@@ -25,7 +34,7 @@ Promise.all(queries)
     console.log('Number of registered voters in Canton: ', results[0].length);
     console.log('Full names of those whose name is STARR: ', results[1].map(p => p.firstname).toString(), results[1].map(p => p.lastname).toString());
     console.log('Number of people who voted in 2016 general election: ', results[2].length);
-    // console.log('Names in order: ', results[0].map(p => p.name));
+    console.log('The last name alphabetically: ', results[3].map(p => p.lastname).toString());
     // console.log('Started most recently: ', results[1].map(p => p.name));
     // console.log('Started in 2003: ', results[2].map(p => p.name));
     // console.log('Teaches 362: ', results[3].map(p => p.name));
