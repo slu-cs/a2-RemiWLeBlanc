90a87af062b7ec15c74d82c0ffe648ebd04496e6
Remi LeBlanc <remwileblanc@gmail.com>
2020-02-22 13:36:12 -0500

    create on each line

--- create.js
+++ create.js
@@ -16,39 +16,17 @@ const file = readline.createInterface({
 // Asynchronous line-by-line input
 file.on('line', function(line) {
   console.log(line);
+  const voter = new Voter(line);
+
+  // reset the data
+  mongoose.connection.dropDatabase()
+    .then(() => voter.save())
+    .then(() => mongoose.connection.close())
+    .then(() => console.log('Database is ready.'))
+    .catch(error => console.error(error.stack));
 });
 
 // End the program when the file closes
 file.on('close', function() {
   process.exit(0);
 });
-// // Create some faculty
-// const harcourt = new Professor({
-//   name: 'Ed Harcourt',
-//   rank: 'Full',
-//   started: 2003,
-//   courses: [140, 220, 345, 362, 364]
-// });
-//
-// const torrey = new Professor({
-//   name: 'Lisa Torrey',
-//   rank: 'Associate',
-//   started: 2009,
-//   courses: [140, 219, 332, 362, 374, 380]
-// });
-//
-// const lee = new Professor({
-//   name: 'Choong-Soo Lee',
-//   rank: 'Associate',
-//   started: 2010,
-//   courses: [140, 219, 256, 321, 370]
-// });
-
-// reset the data
-mongoose.connection.dropDatabase()
-  .then(() => harcourt.save())
-  .then(() => torrey.save())
-  .then(() => lee.save())
-  .then(() => mongoose.connection.close())
-  .then(() => console.log('Database is ready.'))
-  .catch(error => console.error(error.stack));
