7c725183627744a5f9e59e3cb4d1b26027a18e5b
Remi LeBlanc <remwileblanc@gmail.com>
2020-02-22 14:54:58 -0500

    3rd queries

--- query.js
+++ query.js
@@ -14,16 +14,17 @@ query.exec(function(error, professors) {
 });*/
 
 const queries = [
-
   // Voters from Canton
   Voter.find().where('zipcode').equals(13617),
-  Voter.find().where('firstname').equals('STARR')
+  Voter.find().where('firstname').equals('STARR'),
+  Voter.find().where('history').includes('GE16')
 ];
 
 Promise.all(queries)
   .then(function(results) {
     console.log('Number of registered voters in Canton: ', results[0].length);
     console.log('Full names of those whose name is STARR: ', results[1]);
+    console.log('Number of people who voted in 2016 general election: ', results[2].length);
     // console.log('Names in order: ', results[0].map(p => p.name));
     // console.log('Started most recently: ', results[1].map(p => p.name));
     // console.log('Started in 2003: ', results[2].map(p => p.name));
